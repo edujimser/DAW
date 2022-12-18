908f01f144fd92c1c6d02b2955272fa01e9f4e96
nherrera21 <116189436+nherrera21@users.noreply.github.com>
2022-12-18 20:06:45 +0000

    18/12

--- api/api-login/index.api.php
+++ api/api-login/index.api.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
  require './api-fuction.php';
  
@@ -7,16 +6,4 @@
             obtenerDatosUsuario();
             break;
     };
-
-=======
-<?php
- require './api-fuction.php';
- 
-     switch ($_SERVER["REQUEST_METHOD"]) {  
-        case 'GET':
-            obtenerDatosUsuario();
-            break;
-    };
-
->>>>>>> 9c79600374752277b42e60b8e7c92df760a35558
 ?>
