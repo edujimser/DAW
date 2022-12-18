354abae7d2f6c74397c043c79e1a8a8d81a03e2d
edujimser <119799007+edujimser@users.noreply.github.com>
2022-12-18 20:52:26 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,4 +1,15 @@
 <?php
+<?php 
+    require '../controlador/login.php';
+    require './funciones/fucionesExternas.php';
 
+ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['submit'] == 'inicioSession')) {
+    $user = obtenerDatosUsuario($_POST["email"], $_POST['pass']);
+    print_r($user);
+ }
+
+
+require '../views/view.login.php';
+?>
 
 ?>
