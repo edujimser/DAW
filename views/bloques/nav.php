f73a05ef29051fc7ae94e42ab284d39ecc451c8a
edujimser <eduardojimenezserrato@gmail.com>
2022-12-19 01:12:08 +0100

    mas

--- views/bloques/nav.php
+++ views/bloques/nav.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="views/css/estilo.css" type="text/css">
+    <link rel="stylesheet" href="http://127.0.0.1/DAW/views/css/estilo.css" type="text/css">
+    <link rel="stylesheet" href="http://127.0.0.1/DAW/views/css/form.css" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <title>SnapDog</title>
@@ -26,7 +27,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Navbar brand -->
                     <a class="navbar-brand mt-2 mt-lg-0 ms-4" href="./index.php">
-                        HO<img src="./views/img/logo.png" class="nav-logo-home" />ME
+                        HO<img src="http://127.0.0.1/DAW/views/img/logo.png" class="nav-logo-home" />ME
                     </a>
                     <!-- Left links -->
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
