d046ebfb59300fa7d293de5964275ae2a768faca
nherrera21 <116189436+nherrera21@users.noreply.github.com>
2023-01-22 18:53:59 +0000

    add

--- views/view.trabajaconnosotros.php
+++ views/view.trabajaconnosotros.php
@@ -48,5 +48,8 @@
 			</div>
 		</form>
 	</div>
+	<script src="http://127.0.0.1/DAW/js/login.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
 </html>
