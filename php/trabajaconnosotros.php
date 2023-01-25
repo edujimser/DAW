d8e5890b752c3eb22176cb08b0c69d38ffdd6a35
nherrera21 <116189436+nherrera21@users.noreply.github.com>
2023-01-25 20:34:01 +0000

    25/01  +

--- php/trabajaconnosotros.php
+++ php/trabajaconnosotros.php
@@ -25,13 +25,16 @@ $mensaje = $_POST["mensaje"];
 //Mensajes entrantes
 $asunto = "Nuevo mensaje sobre incorporación de empleo recibido";
 
+//Datos para enviar el mensaje 
 $datos = "De: $nombre\nEmail: $email\nMensaje: $mensaje";
 $mensaje = "Se ha recibido un nuevo mensaje sobre una posible incorporación de empleo.:\n$datos";
-$destinatario = "tu_correo@dominio.com"; // persona que recibirá los mensajes
+$destinatario = "tu_correo@dominio.com"; // Persona que recibirá los mensajes
 $encabezados = "Sender: correo@dominio.com\r\n"; // El remitente
 $encabezados .= "From: $nombre <" . $email . ">\r\n";
 $encabezados .= "Reply-To: $nombre <$email>\r\n";
 $resultado = mail($destinatario, $asunto, $mensaje, $encabezados);
+
+//Resultados que nos devuelve, dos opciones, si el mensaje se envío o si hubo algún fallo
 if ($resultado) {
     echo "<h1>Mensaje enviado, ¡Gracias por estar interesado en trabajar con nosotros!</h1>";
     echo "<p>Tu mensaje se ha enviado correctamente.</p><h2>Importante</h2><p>Revisa tu bandeja de SPAM, en ocasiones las respuestas están allí. </p>";
--- views/view.trabajaconnosotros.php
+++ views/view.trabajaconnosotros.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,7 +9,6 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 	<title>Trabaja con nosotros</title>
 </head>
-
 <body>
 	<div class="container">
 		<div class="row">
@@ -59,9 +57,7 @@
 			</div>
 		</div>
 	</div>
-	</div>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
-
 </html>
