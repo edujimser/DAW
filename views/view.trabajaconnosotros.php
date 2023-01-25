7af87e3b78bfd30c3b529602005399e6b4c7473a
nherrera21 <116189436+nherrera21@users.noreply.github.com>
2023-01-25 17:53:25 +0000

    actualizado

--- views/view.trabajaconnosotros.php
+++ views/view.trabajaconnosotros.php
@@ -1,55 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilo.css" type="text/css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <title>Trabaja con nosotros</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/estilo.css" type="text/css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+	<title>Trabaja con nosotros</title>
 </head>
+
 <body>
-	<div class="form-container">
-		<form name="frmContact" id="" frmContact"" method="post" action="">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="well well-sm">
+					<form class="form-horizontal" method="post">
+						<fieldset>
+							<legend class="text-center header">Contacta con nosotros</legend>
 
-			<div class="input-row">
-				<label style="padding-top: 20px;">Nombre</label> <span
-					id="userName-info" class="info"></span><br /> <input type="text"
-					class="input-field" name="userName" id="userName" />
-			</div>
-			<div class="input-row">
-				<label>Email</label> <span id="userEmail-info" class="info"></span><br />
-				<input type="text" class="input-field" name="userEmail"
-					id="userEmail" />
-			</div>
-			<div class="input-row">
-				<label>Asunto</label> <span id="subject-info" class="info"></span><br />
-				<input type="text" class="input-field" name="subject" id="subject" />
-			</div>
-			<div class="input-row">
-				<label>Mensaje </label> <span id="userMessage-info" class="info"></span><br />
-				<textarea name="content" id="content" class="input-field" cols="60"
-					rows="6"></textarea>
-			</div>
-			<div>
-				<input type="submit" name="send" class="btn-submit" value="Send" />
+							<div class="form-group">
+								<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+								<div class="col-md-8">
+									<input id="nombre" name="nombre" type="text" placeholder="Nombre completo" class="form-control">
+								</div>
+							</div>
 
-				<div id="statusMessage"> 
-                        <?php
-                        if (! empty($message)) {
-                            ?>
-                            <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
-                        <?php
-                        }
-                        ?>
-                    </div>
+							<div class="form-group">
+								<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
+								<div class="col-md-8">
+									<input id="email" name="email" type="text" placeholder="Email" class="form-control">
+								</div>
+							</div>
+
+							<div class="form-group">
+								<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+								<div class="col-md-8">
+									<input id="num" name="num" type="text" placeholder="Número de contacto" class="form-control">
+								</div>
+							</div>
+
+							<div class="form-group">
+								<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
+								<div class="col-md-8">
+									<textarea class="form-control" id="mensaje" name="Mensaje" placeholder="Escribe tu mensaje, te responderemos lo antes posible" rows="7"></textarea>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<div class="col-md-12 text-center">
+									<button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+								</div>
+							</div>
+						</fieldset>
+					</form>
+				</div>
 			</div>
-		</form>
-
-
+		</div>
 	</div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+	</div>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
+
 </html>
