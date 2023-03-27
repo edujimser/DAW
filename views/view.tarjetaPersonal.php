209ea06ab60482496c148632c0591ba6489b942a
edujimser <eduardojimenezserrato@gmail.com>
2023-03-27 16:57:57 +0200

    seguridad en tarjeta Personal

--- views/view.tarjetaPersonal.php
+++ views/view.tarjetaPersonal.php
@@ -24,9 +24,8 @@ require 'php/tarjetaPersonal.php';
        </div>
     </div>
  </section>
-<?php endif ?>
-
-<div class='dashboard-app'>
+<?php else: ?>
+    <div class='dashboard-app'>
             <header class='dashboard-toolbar'><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a></header>
             <div class='dashboard-content'>
                 <div class='container'>
@@ -225,6 +224,7 @@ require 'php/tarjetaPersonal.php';
             </div>
         </div>
     </div>
+    <?php endif; ?>
     <script src="http://127.0.0.1/DAW/js/menuUsuario.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 
